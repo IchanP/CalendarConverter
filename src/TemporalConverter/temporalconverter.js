34c8d62e4ed67fd1195b3c99c35bcc533a5aa3b9
Pontus Grandin <pg222pb@student.lnu.se>
2023-09-14 13:53:56 +0200

    define temporalconverter class

---
+++ src/TemporalConverter/temporalconverter.js
@@ -0,0 +1,17 @@
+/**
+ * Wrapper for time and calendar conversion methods.
+ */
+class TemporalConverter {
+  /**
+   * Converts from the Japanese Imperial Year, Kōki, to the Gregorian Calendar.
+   *
+   * Kōki information: https://en.wikipedia.org/wiki/Japanese_imperial_year
+   *
+   * @param {number} year - The Japanese Imperial Year in number format.
+   * @returns {string} - Returns the converted year in "YYYY BCE/CE" format.
+   */
+  KokiToGregorian (year) {
+  }
+}
+
+export default TemporalConverter
