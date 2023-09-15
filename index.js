34fe36757eda6df9b98ea409e5361376f115ab8e
Pontus Grandin <pg222pb@student.lnu.se>
2023-09-15 13:28:19 +0200

    readme update

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 import temporalConverter from './src/TemporalConverter/temporalconverter.js'
 
 export default temporalConverter
-
-console.log(temporalConverter.KokiToGregorian(500))
-console.log(temporalConverter.KokiToGregorian(2660))
