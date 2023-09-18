97e74bb3dd9de7066f02f1dcee6cfeca854a1ba5
Pontus Grandin <pg222pb@student.lnu.se>
2023-09-18 22:21:48 +0200

    Continuity fixes in japanese eras

---
+++ src/tests/eratimes.test.js
@@ -0,0 +1,16 @@
+import { japaneseEras } from '../TemporalConverter/japaneseeras'
+
+describe('Era continuity, japaneseeras.js', () => {
+  test('the eras from 3rd element should have continuity', () => {
+    for (let i = 4; i < japaneseEras.length; i++) {
+      const currentEra = japaneseEras[i]
+      const previousEra = japaneseEras[i - 1]
+      try {
+        expect(currentEra.startYear).toBe(previousEra.endYear)
+        expect(currentEra.startMonth).toBe(previousEra.endMonth)
+      } catch (error) {
+        console.error('Mismatch on era name: ' + currentEra.name)
+      }
+    }
+  })
+})
